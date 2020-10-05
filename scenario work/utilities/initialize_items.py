dead41dab7e0c31936524fc301e970bafde58ed2
Ben <behnam.zakeri@live.com>
2020-10-05 10:36:09 +0200

    a module for initialization of new items added

---
+++ scenario work/utilities/initialize_items.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 09:50:11 2020
+
+@author: zakeri
+"""
+
+# Initializing new sets and parameters if needed
+def init_new_items(sc):
+
+    # 1) Adding sets
+    dict_set = {
+        'is_relation_upper_time': [
+            ['relation', 'node', 'year', 'time'],
+            ['relation', 'node_rel', 'year_rel', 'time']
+            ],
+        'is_relation_lower_time': [
+            ['relation', 'node', 'year', 'time'],
+            ['relation', 'node_rel', 'year_rel', 'time']
+            ],
+        }
+    for item, idxs in dict_set.items():
+        try:
+            sc.init_set(item, idx_sets=idxs[0], idx_names=idxs[1])
+            print('- Set {} was initialized.'.format(item))
+        except:
+            pass
+    # 2) Adding parameters
+    data = {
+        'relation_activity_time': [
+            ['relation', 'node', 'year', 'node', 'technology', 'year',
+             'mode', 'time'],
+            ['relation', 'node_rel', 'year_rel', 'node_loc', 'technology',
+             'year_act', 'mode', 'time']
+            ],
+        'relation_lower_time': [
+            ['relation', 'node', 'year', 'time'],
+            ['relation', 'node_rel', 'year_rel', 'time']
+            ],
+        'relation_upper_time': [
+            ['relation', 'node', 'year', 'time'],
+            ['relation', 'node_rel', 'year_rel', 'time']
+            ],
+        'emission_factor_time': [
+            ['node', 'technology', 'year', 'year', 'mode', 'emission', 'time'],
+            ['node', 'technology', 'year_vtg', 'year_act', 'mode', 'emission',
+             'time'],
+            ],
+        }
+
+    for item, idxs in data.items():
+        try:
+            sc.init_par(item, idx_sets=idxs[0], idx_names=idxs[1])
+            print('- Parameter {} was initialized.'.format(item))
+        except:
+            pass
